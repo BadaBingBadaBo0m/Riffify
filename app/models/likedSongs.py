84f08bc555790f0a5a69c66d4bf35a28631886fb
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-08 15:41:19 -0400

    added enviroment if conditional to liked songs

--- app/models/likedSongs.py
+++ app/models/likedSongs.py
@@ -4,6 +4,9 @@ from datetime import datetime
 class LikedSong(db.Model):
     __tablename__ = 'liked_songs'
 
+    if environment == 'production':
+        __table_args__ = { 'schema': SCHEMA }
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
     song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
