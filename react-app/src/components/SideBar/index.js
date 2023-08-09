a887b248a49800893b5de6f3eb8e43adc6303c0f
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-08 22:46:36 -0400

    main page layout done

--- react-app/src/components/SideBar/index.js
+++ react-app/src/components/SideBar/index.js
@@ -15,7 +15,7 @@ const SideBar = () => {
       </ul>
       <div id='playlist-container'>
         <div id='library-container'>
-          <h2 id='library heading'>Your Library</h2>
+          <h2 id='library-heading'>Your Library</h2>
           <div id='create-first-playlist'>
             <h2>Create your first playlist</h2>
             <p>It's easy, we'll help you</p>
