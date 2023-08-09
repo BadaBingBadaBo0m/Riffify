d8831565f4c28c459536443a0916a47782c3a8ae
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-09 17:11:22 -0400

    album form in place but not connected

--- react-app/src/components/AlbumForm/index.js
+++ react-app/src/components/AlbumForm/index.js
@@ -2,23 +2,60 @@ import React, { useState } from 'react'
 import './albumform.css'
 
 const AlbumForm = () => {
-  const [name, setName] = useState(null)
+  const [name, setName] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [image, setImage] = useState(null);
+  const [artists, setArtists] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventdefault()
+  }
 
   return (
     <div id='album-form-container'>
       <form id='album-form'>
-        <h1>Create a new album</h1>
+        <h1 id='album-form-header'>Create a new album</h1>
 
-        <div id='album-form-input-container'>
+        <div className='album-form-input-container'>
           <label htmlFor='name'>Name</label>
           <input
-            id='album-form-input'
+            className='album-form-input'
             type='text'
             required
             value={name}
           />
-
         </div>
+
+        <div className='album-form-input-container'>
+          <label htmlFor='name'>Description</label>
+          <textarea
+            className='album-form-textarea'
+            type='text'
+            required
+            value={description}
+          />
+        </div>
+
+        <div className='album-form-input-container'>
+          <label htmlFor='name'>Album art</label>
+          <input
+            className='album-form-file'
+            type='file'
+            accept="image/*"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
+        </div>
+
+        <button id='album-form-submit' onSubmit={handleSubmit}>Submit</button>
+
+        {/* <div className='album-form-input-container'>
+          <label htmlFor='name'>Featured artists</label>
+          <input
+            className='album-form-input'
+            type='text'
+            value={artists}
+          />
+        </div> */}
 
       </form>
     </div >
