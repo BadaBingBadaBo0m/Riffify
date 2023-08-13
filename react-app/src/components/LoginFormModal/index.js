59b39a957525174823e83f0d9336b5f7d66bd155
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-13 00:17:30 -0400

    styled login modal

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -11,46 +11,63 @@ function LoginFormModal() {
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
+  const handleDemoLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login('currents@aa.io', 'password'));
+    if (data) {
+      setErrors(data);
+    } else {
+      closeModal()
+    }
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
     if (data) {
       setErrors(data);
     } else {
-        closeModal()
+      closeModal()
     }
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
+    <div id="login-form-container">
+      <h1>Log in to Tritone</h1>
+      <form onSubmit={handleSubmit} id="login-form">
+        <ul id="login-form-errors">
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <label>
-          Email
+
+        <div className="login-inputs-labels">
+          <label className="login-labels">Email</label>
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className='login-inputs'
             required
           />
-        </label>
-        <label>
-          Password
+        </div>
+
+        <div className="login-inputs-labels">
+          <label className="login-labels">Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className='login-inputs'
             required
           />
-        </label>
-        <button type="submit">Log In</button>
+        </div>
+
+        <button id="login-form-button" type="submit">Log In</button>
+
+        <button id="login-demo-button" type="submit" onClick={handleDemoLogin}>Demo user</button>
       </form>
-    </>
+    </div>
   );
 }
 
