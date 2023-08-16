659c6ca58af0418c58818b9a95ecbefeff5cd4b2
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-15 20:57:53 -0400

    sign up validations

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -12,22 +12,63 @@ function SignupFormModal() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
+
+  const validateEmail = (email) => {
+    return email.match(
+      // eslint-disable-next-line
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+  const handleFlaskErrors = (errorsList) => {
+    let errorObj = {}
+    for (let i = 0; i < errorsList.length; i++) {
+      const splitError = errorsList[i].split(':')
+      errorObj[splitError[0].trim()] = splitError[1]
+    }
+    // console.log('obj', errorObj)
+    setErrors(errorObj)
+    console.log(errorsList)
+  }
+
+  const validateData = () => {
+    const errorObj = {}
+
+    if (!validateEmail(email)) errorObj.email = ' Must be valid'
+    // if (validateEmail(email)) delete errorObj.email
+    // if (!username) errorObj.username = ' Field is required'
+    if (username.length < 5) errorObj.username = ' Must be at least 5 characters'
+    if (username.length > 10) errorObj.username = ' Must be shorter than 10 characters'
+
+    // if (!firstName) errorObj.firstName = ' Field is required'
+    if (firstName.length < 2) errorObj.firstName = ' Must be at least 2 characters'
+    if (firstName.length > 10) errorObj.firstName = ' Must be shorter than 10 characters'
+
+    // if (!lastName) errorObj.lastName = ' Field is required'
+    if (lastName.length < 2) errorObj.lastName = ' Must be at least 2 characters'
+    if (lastName.length > 10) errorObj.lastName = ' Must be shorter than 10 characters'
+
+    if (password.length < 6) errorObj.password = ' Must be at least 6 characters'
+    if (password.length > 15) errorObj.password = ' Must be shorter than 15 characters'
+
+    setErrors(errorObj)
+    console.log(errorObj)
+    if (!Object.keys(errorObj).length) return true
+    else return false
+
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
+    if (validateData()) {
       const data = await dispatch(signUp(username, firstName, lastName, email, password));
       if (data) {
-        setErrors(data);
+        handleFlaskErrors(data)
       } else {
-        closeModal();
+        closeModal()
       }
-    } else {
-      setErrors([
-        "Confirm Password field must be the same as the Password field",
-      ]);
     }
   };
 
@@ -35,14 +76,9 @@ function SignupFormModal() {
     <div id="signUp-form-container">
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit} id="signUp-form">
-        <ul id="signUp-form-errors">
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
 
         <div className="signUp-inputs-labels">
-          <label className="signUp-labels">Email</label>
+          <label className="signUp-labels">Email{errors.email && <span>:</span>}{errors.email && <span className="errors">{errors.email}</span>}</label>
           <input
             type="text"
             value={email}
@@ -53,7 +89,7 @@ function SignupFormModal() {
         </div>
 
         <div className="signUp-inputs-labels">
-          <label className="signUp-labels">Username</label>
+          <label className="signUp-labels">Username{errors.username && <span>:</span>}{errors.username && <span className="errors">{errors.username}</span>}</label>
           <input
             type="text"
             value={username}
@@ -63,9 +99,14 @@ function SignupFormModal() {
           />
         </div>
 
+        <div id="first-last-name-errors-container">
+          {errors.firstName && <span className="errors">First name:{errors.firstName}</span>}
+          {errors.lastName && <span className="errors">Last name:{errors.lastName}</span>}
+        </div>
+
         <div id="first-last-name">
           <div>
-            <label className="first-name-label">Fist name</label>
+            <label className="first-name-label">First name</label>
             <input
               type="text"
               value={firstName}
@@ -88,7 +129,7 @@ function SignupFormModal() {
         </div>
 
         <div className="signUp-inputs-labels">
-          <label className="signUp-labels">Password</label>
+          <label className="signUp-labels">Password{errors.password && <span className="errors">{errors.password}</span>}</label>
           <input
             type="password"
             value={password}
